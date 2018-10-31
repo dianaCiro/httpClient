20c190ba1199ddb31c0c9b45f721dbcb8d729fd9
dianaciro94 <diana.ciro@ceiba.com.co>
2018-10-31 09:00:54 -0500

    consumo servicio rest desde java

--- consumorest/src/main/java/com/example/consumorest/ConsumorestApplication.java
+++ consumorest/src/main/java/com/example/consumorest/ConsumorestApplication.java
@@ -17,47 +17,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 public class ConsumorestApplication {
 
-	static String url =
-	         "http://localhost:8080/request";
-	
+	static String url = "http://localhost:8080/request";
+
 	public static void main(String[] args) throws ClientProtocolException, IOException {
 		SpringApplication.run(ConsumorestApplication.class, args);
-		/*HttpClient client = new HttpClient();
-		GetMethod method = new GetMethod(url);
-		method.setRequestHeader("Content-type",
-                "text/xml; charset=ISO-8859-1");
-		 try{
-	            int statusCode = client.executeMethod(method);
 
-	            System.out.println("Status Code = "+statusCode);
-	            System.out.println("QueryString>>> "+method.getQueryString());
-	            System.out.println("Status Text>>>"
-	                  +HttpStatus.getStatusText(statusCode));
-
-	            //Get data as a String
-	            List<Request> requests = List<Request> method.getResponseBody();
-	            System.out.println(method.getResponseBodyAsString());
-
-	          
-	            //release connection
-	            method.releaseConnection();
-	        }
-	        catch(IOException e) {
-	            e.printStackTrace();
-	        }*/
-		
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpGet request = new HttpGet(url);
 
-		// add request header
 		HttpResponse response = client.execute(request);
 
-	System.out.println("Response Code : " 
-	                + response.getEntity());
-	ObjectMapper mapper = new ObjectMapper();
-	 
-	
-	 List<Request> requests = mapper.readValue( response.getEntity().getContent(), mapper.getTypeFactory().constructCollectionType(List.class, Request.class));
+		ObjectMapper mapper = new ObjectMapper();
+
+		List<Request> requests = mapper.readValue(response.getEntity().getContent(),
+				mapper.getTypeFactory().constructCollectionType(List.class, Request.class));
 
 		System.out.println(requests.get(2));
 	}
